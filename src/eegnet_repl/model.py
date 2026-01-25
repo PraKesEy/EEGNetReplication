870bad09e8d74b10379cbf2fc1aef59c0899c655
Prashastha Mudannayake <prashasthamudannayake98@gmail.com>
2026-01-25 08:06:31 +0200

    [Model] Model archtiecture and the train, test function

--- src/eegnet_repl/model.py
+++ src/eegnet_repl/model.py
@@ -6,38 +6,233 @@ from dataclasses import dataclass
 from typing import Any
 
 import numpy as np
-from sklearn.impute import SimpleImputer
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-
+import torch
+import torch.nn as nn
 
 @dataclass(frozen=True)
-class TrainedModel:
-    """Bundle for a trained model and metadata."""
+# Define model
+class EEGNet(nn.Module):
+    def __init__(self, C, T, F1=8, D=2, p=0.5):
+        # C = number of channels, input.shape[1], C = 22 based on 02_preprocessing_pipeline
+        # T = number of timepoints per batch, input.shape[2], T = 257 based on 02_preprocessing_pipeline
+        # F1 = temporal filters
+        # D = spatial filters
+        # p = dropout probability: 0.5 for within-subject classification, 0.25 for cross-subject classification
 
-    pipeline: Pipeline
-    feature_cols: list[str]
-    target_col: str
+        super().__init__()
+        F2 = F1*D # pointwise convolutions, can be any number but authors choose this
+        self.temporal = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=F1,
+                kernel_size=(1,32),
+                padding='same', # keeps initial dims per channel
+                groups=1, # default
+                bias=False # "We omit the use of bias units in all convolutional layers."
+            ),
+            # Linear activation = do nothing?
+            nn.BatchNorm2d(num_features=F1)) # "We apply batch normalization along the feature map dimension"
+        
+        self.spatial = nn.Conv2d(
+            in_channels=F1,
+            out_channels=D*F1,
+            kernel_size=(C,1), 
+            padding='valid', # no padding -> collapses channel dimension
+            groups=F1, # produces depthwise convolution
+            bias=False # "We omit the use of bias units in all convolutional layers."
+            )
+        # "We also regularize each spatial filter by using a maximum norm constraint of 1 on its weights"
+        max_norm_value = 1.0 
+        self.spatial.weight.register_hook(lambda x: torch.clamp(x, min=-max_norm_value, max=max_norm_value))
 
-    def predict_proba_hazard(self, X: Any) -> np.ndarray:
-        """Return probability of class=1 (hazardous)."""
-        proba = self.pipeline.predict_proba(X)
-        # assuming classes [0,1]
-        return proba[:, 1]
+        self.aggregation = nn.Sequential(
+            nn.BatchNorm2d(num_features=D*F1), # "We apply batch normalization along the feature map dimension"
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=(1,4)), # default stride = kernel_size 
+            nn.Dropout(p=p)
+        )
+
+        self.block_2 = nn.Sequential(
+            nn.Conv2d( # depthwise part of separable convolution
+            in_channels=D*F1,
+            out_channels=D*F1,
+            kernel_size=(1,16), 
+            padding='same', # keeps initial dims per channel
+            groups=D*F1, # produces depthwise convolution
+            bias=False # "We omit the use of bias units in all convolutional layers."
+            ),
+            nn.Conv2d( # pointwise part of separable convolution
+            in_channels=D*F1,
+            out_channels=F2,
+            kernel_size=(1,1), 
+            padding='same', # keeps initial dims per channel, shouldn't matter in this case
+            groups=1, # default
+            bias=False # "We omit the use of bias units in all convolutional layers."
+            ),
+            # Linear activation = do nothing?
+            nn.BatchNorm2d(num_features=F2), # "We apply batch normalization along the feature map dimension"
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=(1,8)), # default stride = kernel_size
+            nn.Dropout(p=p),
+            nn.Flatten()
+        )
+
+        self.classifier = nn.Linear(
+                in_features=F2*(T//32),
+                out_features=4, # number of classes
+                bias=True # since it's not a convolutional layer..?
+            )
+        max_norm_value_1 = 0.25 
+        self.classifier.weight.register_hook(lambda x: torch.clamp(x, min=-max_norm_value_1, max=max_norm_value_1))
+        
+        # nn.Softmax(dim=1) # dim = 0 is for batch number; comented out because of nn.CrossEntropyLoss() documentation:
+        # "The input is expected to contain the unnormalized logits for each class (which do not need to be positive or sum to 1, in general)."
 
 
-def build_pipeline(seed: int) -> Pipeline:
-    """Create a simple, strong baseline pipeline."""
-    clf = LogisticRegression(
-        max_iter=2000,
-        class_weight="balanced",
-        random_state=seed,
-    )
-    return Pipeline(
-        steps=[
-            ("impute", SimpleImputer(strategy="median")),
-            ("scale", StandardScaler()),
-            ("clf", clf),
-        ],
-    )
+
+    def forward(self, x):
+        x = torch.unsqueeze(x,dim=1) # inserts new dim at specified position, shape = (n_batches, C, T) -> shape = (n_batches, 1, C, T)
+        filter_bank = self.temporal(x)
+        spatial_pattern = self.spatial(filter_bank)
+        block_1_out = self.aggregation(spatial_pattern)
+        block_2_out = self.block_2(block_1_out)
+        output = self.classifier(block_2_out)
+        
+        return output
+    
+def train(model, optimizer, loss_fn, train_loader, val_loader, nepochs=500):
+    '''
+    Train a pytorch model and evaluate it every epoch.
+    Params:
+    model - a pytorch model to train
+    optimizer - an optimizer
+    loss_fn - the criterion (loss function)
+    nepochs - number of training epochs
+    train_loader - dataloader for the trainset
+    val_loader - dataloader for the valset
+
+    Returns: 
+    1. state_dict of the model with the lowest validation loss, 
+    2. list of train losses,
+    3. list of validation losses.
+    '''
+    
+    # Detect device and move model to it
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    print(f"Training on {device} device")
+    
+    # Move model to device
+    model = model.to(device)
+    
+    
+    train_losses, val_losses = [], []
+    best_model = model.state_dict()
+
+    for e in range(nepochs):
+        running_loss = 0
+        running_val_loss = 0
+        for signals, labels in train_loader: # signals = (batch, C, T), labels = (batch, label)
+
+            # Map the old values (7, 8, 9, 10) to new values (0, 1, 2, 3) -> need to change data processing later
+            new_labels = torch.zeros_like(labels)
+            new_labels[labels == 7] = 0
+            new_labels[labels == 8] = 1
+            new_labels[labels == 9] = 2
+            new_labels[labels == 10] = 3
+            
+            signals = signals.float() # added to avoid dtype mismatch error
+
+            # Move data to device
+            signals, new_labels = signals.to(device), new_labels.to(device)
+
+            # Training pass
+            model.train() # set model in train mode
+            preds = model(signals)
+            loss = loss_fn(preds,new_labels)
+
+            running_loss += loss.item()
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+    #else:
+        val_loss = 0
+        # Evalaute model on validation at the end of each epoch.
+        with torch.no_grad():
+            for signals, labels in val_loader: # signals = (batch, C, T), labels = (batch, label)
+
+                # Map the old values (7, 8, 9, 10) to new values (0, 1, 2, 3) -> need to change data processing later
+                new_labels = torch.zeros_like(labels)
+                new_labels[labels == 7] = 0
+                new_labels[labels == 8] = 1
+                new_labels[labels == 9] = 2
+                new_labels[labels == 10] = 3
+
+                signals = signals.float() # added to avoid dtype mismatch error
+                
+                # Move data to device
+                signals, new_labels = signals.to(device), new_labels.to(device)
+                
+                preds = model(signals)
+                val_loss = loss_fn(preds,new_labels)
+
+                running_val_loss += val_loss.item()
+
+        # track train loss and validation loss
+        train_losses.append(running_loss/len(train_loader))
+        val_losses.append(running_val_loss/len(val_loader))
+
+        if running_val_loss == np.min(np.array(val_losses)):
+            best_model = model.state_dict()
+
+        if e%50==0:
+            print("Epoch: {}/{}.. ".format(e+1, nepochs),
+            "Training Loss: {:.3f}.. ".format(running_loss/len(train_loader)),
+            "Validation Loss: {:.3f}.. ".format(running_val_loss/len(val_loader)))
+
+    return best_model, train_losses, val_losses
+
+def test(model, test_loader, loss_fn) -> float:
+    '''
+    Test a pytorch model.
+    Params:
+    model - a pytorch model to test
+    test_loader - dataloader for the testset
+    loss_fn - the criterion (loss function)
+
+    Returns: 
+    test loss
+    '''
+    
+    # Detect device and move model to it
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    print(f"Testing on {device} device")
+    
+    # Move model to device
+    model = model.to(device)
+    
+    test_loss = 0
+
+    with torch.no_grad():
+        for signals, labels in test_loader: # signals = (batch, C, T), labels = (batch, label)
+
+            # Map the old values (7, 8, 9, 10) to new values (0, 1, 2, 3) -> need to change data processing later
+            new_labels = torch.zeros_like(labels)
+            new_labels[labels == 7] = 0
+            new_labels[labels == 8] = 1
+            new_labels[labels == 9] = 2
+            new_labels[labels == 10] = 3
+
+            signals = signals.float() # added to avoid dtype mismatch error
+            
+            # Move data to device
+            signals, new_labels = signals.to(device), new_labels.to(device)
+            
+            preds = model(signals)
+            loss = loss_fn(preds,new_labels)
+
+            test_loss += loss.item()
+
+    return test_loss/len(test_loader)
